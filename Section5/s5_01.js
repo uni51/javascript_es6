2ea7e6c26202dcc033d4abd71319d82b99554bdf
uni51 <uni51@users.noreply.github.com>
2020-08-19 16:31:23 +0900

    13. 特定のデータを探すfind

---
+++ Section5/s5_01.js
@@ -0,0 +1,21 @@
+var users = [
+  { name: "太郎" },
+  { name: "次郎" },
+  { name: "三郎" },
+  { name: "次郎", id: 10 },
+];
+
+var user;
+
+// for (var i = 0; i < users.length; i++) {
+//   if (users[i].name == "次郎") {
+//     user = users[i];
+//     break;
+//   }
+// }
+
+user = users.find(function (user) {
+  return user.name == "次郎";
+});
+
+console.log(user);
