9c9ccb92486fe3ec9c21dc0ed398b5e1c60ed46c
uni51 <uni51@users.noreply.github.com>
2020-08-19 22:15:30 +0900

    コーディング演習13: 通信中のリクエストの確認

---
+++ Section06/exercise_12.js
@@ -0,0 +1,11 @@
+var users = [
+  { id: 21, hasSubmitted: true },
+  { id: 62, hasSubmitted: false },
+  { id: 4, hasSubmitted: true },
+];
+
+var hasSubmitted;
+
+hasSubmitted = users.every(function (user) {
+  return user.hasSubmitted === true;
+});
